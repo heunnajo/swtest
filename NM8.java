b87b48ac8b10735807f09567bf4d55ae51c34fae
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-08-01 17:29:27 +0900

    NM8 transformed NM4.
    Updated part
    1. order.add(i) => order.add(num[i])
    MISTAKE!!
    order.add(num[start]) => result : 2 2 2 (just same number will be repeated!)
    In For statement) i starts from "start".
    When calling recursion, it starts from current "i" NOT "start"!

---
+++ NM8.java
@@ -0,0 +1,49 @@
+package ss;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+public class NM8 {
+	static int N,M;//N개의 정수를 중복없이 M개, 비내림차순! 12 13 14 22 23 24 ...
+	static StringBuilder sb;
+	static int[] num;
+	static void go(int index,int start,ArrayList<Integer> order) {
+		//정답 찾은 경우 :현재까지 선택한 순열 order를 StringBuilder에 차곡차곡 저장한다!
+		if(index == M) {
+			for(int i=0;i<M;i++) {//order의 0번째,1번째를 sb에 저장.
+				sb.append(order.get(i)+" ");
+				if(i==M-1) {
+					sb.append("\n");
+				}
+			}
+			return;
+		}
+		//불가능한 경우
+		if(start>N-1) {//더 선택할 숫자가 없는 경우:배열 인덱스가 N-1까지기 때문에!!
+			return;
+		}
+		//다음 경우 호출:N개 정수 중에서 index번째 정수를 선택한다. 총 M개 선택!
+		//순열 관점!
+		for(int i=start;i<N;i++) {
+			order.add(num[i]);
+			go(index+1,i,order);
+			order.remove(order.size()-1);
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+		N = sc.nextInt();
+		M = sc.nextInt();
+		ArrayList<Integer> order = new ArrayList<Integer>();
+		num = new int[N];
+		for(int i=0;i<N;i++) {
+			num[i] = sc.nextInt();
+		}
+		Arrays.sort(num);
+		go(0,0,order);//index,start 
+		
+		//StringBuilder에 숫자를 저장해서 한번에 출력한다!
+		System.out.print(sb);
+	}
+
+}
