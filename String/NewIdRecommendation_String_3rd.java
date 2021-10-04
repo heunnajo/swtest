1c1c4cabc476a27691c6d4ef1c60ae2f0018e489
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-10-04 21:24:08 +0900

    Update NewIdRecommendation_String_3rd.java

--- String/NewIdRecommendation_String_3rd.java
+++ String/NewIdRecommendation_String_3rd.java
@@ -1,48 +1,56 @@
 class NewIdRecommendation_String_3rd {
     public String solution(String new_id) {
+        String answer;
+        //1단계
         String step1 = new_id.toLowerCase();
-        
-        String step2 = "";
-        for(int i=0;i<step1.length();i++){
-            if(step1.charAt(i)>='a' && step1.charAt(i)<='z' ||
-              step1.charAt(i)>='0' && step1.charAt(i)<='9' ||
-              step1.charAt(i)=='-' || step1.charAt(i)=='_' || step1.charAt(i)=='.'){
-                step2 += step1.charAt(i);
+        //2단계:for-each문을 위해 step1을 배열로 변환
+        char[] step1_arr = step1.toCharArray();
+        StringBuilder step2 = new StringBuilder();
+        for(char c:step1_arr){
+            if(c>='a' && c<='z' || c>='0' && c<='9' ||c=='-' || c=='_' || c=='.'){
+                step2.append(c);
             }
         }
-        String step3 = "";
-        //step3 = step2.replace("..",".");
-        while(step2.contains("..")){
+        //3단계:String 메서드를 사용하기 위해 2단계의 StringBuilder를 String으로 변환
+        String step3 = step2.toString().replace("..",".");
+        while(step3.contains("..")){
             step3 = step3.replace("..",".");
         }
-        String step4 = "";
-        if(step3.charAt(0) == '.'){
-            step4 = step3.substring(1,step3.length());
-        } 
-        if(step3.charAt(step3.length()-1) == '.'){
-            step4 = step3.substring(0,step3.length()-1);
+        //4단계
+        String step4 = step3;
+        if(step4.length()>0){
+            if(step4.charAt(0) == '.'){
+                step4 = step4.substring(1,step4.length());
+            } 
         }
-        
-        String step5 = "";
-        if(step4.length()==0){
+        if(step4.length()>0){
+            if(step4.charAt(step4.length()-1) == '.'){
+                step4 = step4.substring(0,step4.length()-1);
+            }
+        }
+        //5단계
+        String step5 = step4;
+        if(step5.length()==0){
             step5 += "a";
         }
-        String step6 = "";
-        if(step5.length()>=16){
-            step6 = step5.substring(0,15);//0-14
+        //6단ㄴ계
+        String step6 = step5;
+        if(step6.length()>=16){
+            step6 = step6.substring(0,15);//0-14
             if(step6.charAt(step6.length()-1) == '.'){
                 step6 = step6.substring(0,step6.length()-1);
             }
         }
-        //String step7 = "";
-        if(step6.length()<=2){
-            char c = step6.charAt(step6.length()-1);
-            while(step6.length()<3){
-                step6 += c;
+        StringBuilder step7 = new StringBuilder(step6);
+        if(step7.length()<=2){
+            char c = step6.charAt(step7.length()-1);
+            while(step7.length()<3){
+                step7.append(c);
             }
         }
-        //step7 = step6;
-        //return step7;
-        return step6;
+        
+        //answer = String.valueOf(step7);
+        answer = step7.toString();
+        return answer;
     }
 }
