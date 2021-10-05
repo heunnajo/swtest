ae19ba03aa731c814c1e79eab6bfb33818589ead
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-10-05 14:30:54 +0900

    Update LadderControl_2nd.java

--- LadderControl_2nd.java
+++ LadderControl_2nd.java
@@ -1,4 +1,3 @@
-package ss;
 import java.io.*;
 import java.util.*;
 public class LadderControl_2nd {
@@ -6,45 +5,36 @@ public class LadderControl_2nd {
 	static final int INF = Integer.MAX_VALUE;
 	static int START = 1;
 	static int END = 2;
-	static void go(int cnt,int pos) {
-//	static int go(int cnt,int pos) {
-		//1.재귀 종료
+	static int go(int cnt,int pos) {
+		//1.재귀 종료 :정답 찾은 경우 + 불가능한 경우
 		//1-1.정답 찾은 경우:현재의 cnt를 ans에 저장하고 바로 리턴.
-		if(check()) {
-			ans = cnt;
-			return;
-		}
-		//1-2.불가능한 경우
-//		if(pos>N*H-2) return;
-		if(pos>=N*H) return;
-		if(cnt >= 3) {
+		if(cnt == 3 || pos >= H*N) {
 			if(check()) {
-				ans = cnt;
-				return;
+				return cnt;
 			}
-			else return;//-1?0?
+			return INF;
 		}
 		//2.현재 위치에서 사다리 위치 선택
+		int ret = INF;
 		int curX = pos/N; int curY = pos%N;
-		if(Map[curX][curY] == 0 && Map[curX][curY] == 0 && curY<=N-2) {
+		if(curY<=N-2 && Map[curX][curY] == 0 && Map[curX][curY+1] == 0) {
 			Map[curX][curY] = START; Map[curX][curY+1] = END;
-			go(cnt+1,pos+2);
-			Map[curX][curY] = Map[curX][curY] = 0;//원복처리
+			ret = Math.min(go(cnt+1,pos+2),ret);
+			Map[curX][curY] = Map[curX][curY+1] = 0;//원복처리
 		}
-		go(cnt,pos+1);
+		ret = Math.min(go(cnt,pos+1),ret);
+		return ret;
 	}
-	//사다리 추가한 후 결과 체크
 	static boolean check() {
-		int start,row;
+		int col,row;//난 처음 시작좌표를 start에 저장하고 반복변수인 j로 +- 했는데 정답은 그 반대였다.
 		for(int j=0;j<N;j++) {
-			start = j; row = 0;
+			col = j; row = 0;
 			while(row<H) {
-				if(Map[row][j] == START) j++;
-				if(Map[row][j] == END) j--;
+				if(Map[row][col] == START) col++;
+				else if(Map[row][col] == END) col--;
 				row++;
 			}
-			if(j==start) continue;
-			else if(j!= start) return false;
+			if(j!=col) return false;
 		}
 		return true;
 	}
@@ -63,17 +53,9 @@ public class LadderControl_2nd {
 			int e = Integer.parseInt(st.nextToken())-1;
 			Map[s][e] = START; Map[s][e+1] = END;
 		}
-		ans = INF;
-		go(0,0);
-		if(ans == INF) ans = -1;
-		System.out.println(ans);
-		
+		ans = go(0,0);//최솟값을 리턴
+		if(ans == INF) System.out.println(-1);
+		else System.out.println(ans);
 	}
 
 }
-//for(int i=0;i<H;i++) {
-//	for(int j=0;j<N;j++) {
-//		System.out.print(Map[i][j]+" ");
-//	}
-//	System.out.println();
-//}
