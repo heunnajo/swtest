4220f4a0325142a92dc8088aad68dccf8f79e8f7
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-10-07 19:56:59 +0900

    Update Make1.java

--- Make1.java
+++ Make1.java
@@ -1,26 +1,27 @@
-package ss;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-public class Make1 {
-	
+public class Main {
+	static int[] d;
+	static int go(int n) {
+		if(n==1) return 0;
+		if(d[n]>0) return d[n];
+		d[n] = go(n-1) +1;
+		if(n%2 == 0) {
+			int temp = go(n/2)+1;
+			if(d[n]>temp) d[n] = temp;
+		}
+		if(n%3 == 0) {
+			int temp = go(n/3)+1;
+			if(d[n]>temp) d[n] = temp;
+		}
+		return d[n];
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int cnt = 0;
-		while(N!=1) {
-			cnt++;
-			System.out.println("연산 전 N:"+N);
-			N -= 1;
-			if(N%3 == 0) {
-				N = N/3;
-			} else if(N%2 == 0) {
-				N = N/2;
-			}
-			System.out.println("연산 후 N:"+N);
-			
-		}
-		System.out.println(cnt);
+		d = new int[N+1];
+		System.out.println(go(N));
 	}
 
 }
