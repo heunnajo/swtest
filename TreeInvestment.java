4ef54e3e0b7aabb9ebb91e966fa3beadd66f9879
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-10-07 10:41:54 +0900

    Update TreeInvestment.java
    1. for loop starts from size-1 th index NOT from 0 and added new elements using add().
    2. Added K while loop.

--- TreeInvestment.java
+++ TreeInvestment.java
@@ -27,13 +27,12 @@ public class TreeInvestment {
 				Trees[i][j] = new LinkedList<Integer>();
 			}
 		}
-		for(int i=0;i<N;i++) 
-			for(int j=0;j<N;j++)Nutrition[i][j] = 5;
 		//S2D2 양분 추가값 입력 저장
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<N;j++) {
 				S2D2[i][j] = Integer.parseInt(st.nextToken());
+				Nutrition[i][j] = 5;
 			}
 		}
 		//M개의 나무 정보 저장!
@@ -50,42 +49,46 @@ public class TreeInvestment {
 		System.out.println(ans);
 	}
 	static void fourSeason() {
-		//봄,여름
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				int size = Trees[i][j].size(); int diedAgeSum = 0;
-				for(int k=0;k<size;k++) {
-					int cur = Trees[i][j].get(k);
-					if(Nutrition[i][j] - cur>=0) {//봄
-						Nutrition[i][j] -= cur;//나이만큼 양분 감소
-						Trees[i][j].set(k, cur+1);//나이 1증가
-					} else {//k번째에서 양분부족하면 뒤의 나무들도 다 부족한 것이기 때문.k~끝까지 else문을 처리해야함.
-						diedAgeSum += Trees[i][j].remove(k);
-						k--;
-					}
-				}
-				//(i,j)에 있는 나무 봄,여름 처리하고 난 후! 죽은 나무들 양분으로 합산.
-				//죽는 나무가 없으면 Nutri[i][j]에 더해지는 양분값은 0이 되어 더해지는 양분값 없어진다.
-				Nutrition[i][j] += diedAgeSum/2;
-			}
-		}
-		//가을,겨울
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				//가을 : 나이 5배수 이상인 것들 인접 8개 위치에 나이 1인 나무 생성 
-				int size = Trees[i][j].size();
-				for(int k=0;k<size;k++) {
-					int cur = Trees[i][j].get(k);
-					if(cur%5 == 0) {
-						for(int dir = 0;dir<8;dir++) {
-							int nx = i+dx[dir]; int ny = j+dy[dir];
-							if(isOut(nx,ny)) continue;
-							Trees[nx][ny].addFirst(1);
+		while(K-->0) {
+			//봄,여름
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<N;j++) {
+					int size = Trees[i][j].size(); int diedAgeSum=0;
+					for(int k=size-1;k>=0;k--) {
+						int cur = Trees[i][j].get(k);
+						if( Nutrition[i][j] - cur>=0) {//봄
+							Nutrition[i][j] -= cur;//나이만큼 양분 감소
+							Trees[i][j].set(k, cur+1);//나이 1증가
+						} else {//k번째에서 양분부족하면 뒤의 나무들도 다 부족한 것이기 때문.k~끝까지 else문을 처리해야함.
+							//Nutrition[i][j] += cur/2;
+							diedAgeSum += cur;
+							Trees[i][j].remove(k);
 						}
 					}
+					//(i,j)에 있는 나무 봄,여름 처리하고 난 후! 죽은 나무들 양분으로 합산.
+					//죽는 나무가 없으면 Nutri[i][j]에 더해지는 양분값은 0이 되어 더해지는 양분값 없어진다.
+					//죽은 나무들 나이 합한 것/2 양분 업데이트 한번에 처리!
+					Nutrition[i][j] += diedAgeSum/2;
 				}
-				//겨울 
-				Nutrition[i][j] += S2D2[i][j];
+			}
+			//가을,겨울
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<N;j++) {
+					//가을 : 나이 5배수 이상인 것들 인접 8개 위치에 나이 1인 나무 생성 
+					int size = Trees[i][j].size();
+					for(int k=0;k<size;k++) {
+						int cur = Trees[i][j].get(k);
+						if(cur%5 == 0) {
+							for(int dir = 0;dir<8;dir++) {
+								int nx = i+dx[dir]; int ny = j+dy[dir];
+								if(isOut(nx,ny)) continue;
+								Trees[nx][ny].add(1);
+							}
+						}
+					}
+					//겨울 
+					Nutrition[i][j] += S2D2[i][j];
+				}
 			}
 		}
 	}
@@ -103,14 +106,3 @@ public class TreeInvestment {
 		return sum;
 	}
 }
-//for(int i=0;i<N;i++) {
-//	for(int j=0;j<N;j++) {
-//		if(Trees[i][j].size()==0) System.out.print(0+" ");
-//		int size = Trees[i][j].size();
-//		for(int k=0;k<size;k++) {
-//			int cur = Trees[i][j].get(k);
-//			System.out.print(cur+" ");
-//		}
-//	}
-//	System.out.println();
-//}
