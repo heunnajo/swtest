b43baebfd7ab0df2c4ab0800e714f6c0f17e0e06
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-10-07 20:22:14 +0900

    Update Tiling_2N.java

--- Dynamic Programming/Tiling_2N.java
+++ Dynamic Programming/Tiling_2N.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Tiling_2N {
 	static int dp[];
 	static int go(int N) {
-		if(N<=0) return 0;
-		if(dp[N]>0) return 0;//이미 채워진 경우
+		if(N==0) dp[N]=1;
+		else if(N==1) dp[N]=1;
+		if(dp[N]>0) return dp[N];//이미 채워진 경우
 		
-		//dp[N] = dp[N-1] + dp[N-2];
-		dp[N] = go(N-1) + go(N-2);
+		dp[N] = go(N-1)%10007 + go(N-2)%10007;
 		return dp[N];
 	}
 	public static void main(String[] args) {
@@ -15,11 +15,7 @@ public class Tiling_2N {
 		int N = sc.nextInt();
 		dp = new int[1001];
 		
-		dp[0] = 0;
-		dp[1] = 1;
-		dp[2] = 2;
-		go(N);
-		System.out.println(dp[N]);
+		System.out.println(go(N)%10007);
 	}
 
 }
