b66770044a548114a4e6fc92b2962a80bf1f63f8
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-10-20 09:03:43 +0900

    Update EditingTable.java
    Same correct result when accessing prev and next properties of "cur" instance directly and using reference variables.

--- EditingTable.java
+++ EditingTable.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class EditingTable {
+class Solution {
     static class Node{
         boolean removed;
         Node prev;
@@ -13,26 +13,26 @@ class EditingTable {
         //prev,next 링크 초기화!:n개의 행에 대해.
         for(int i=1;i<n;i++){
             NodeArr[i].prev = NodeArr[i-1];
-            NodeArr[i].next = NodeArr[i+1];
+            NodeArr[i-1].next = NodeArr[i];
         }
         
         Node cur = NodeArr[k];
         Stack<Node> mystack = new Stack<>();
         
         for(int i=0;i<cmd.length;i++){//현재의 명령어 = cmd[i]
-            //String str = cmd[i];
-            if(cmd[i].charAt(0) == 'U'){
-                int num = Integer.parseInt(cmd[i].susbstring(2));
+            String str = cmd[i];
+            if(str.charAt(0) == 'U'){
+                int num = Integer.parseInt(str.substring(2));
                 for(int j=0;j<num;j++)
                     cur = cur.prev;
-            } else if(cmd[i].charAt(0) == 'D'){
-                int num = Integer.parseInt(cmd[i].susbstring(2));
+            } else if(str.charAt(0) == 'D'){
+                int num = Integer.parseInt(str.substring(2));
                 for(int j=0;j<num;j++)
                     cur = cur.next;
-            } else if(cmd[i].charAt(0) == 'C'){//현재 행 삭제!
+            } else if(str.charAt(0) == 'C'){//현재 행 삭제!
                 cur.removed = true;
                 mystack.push(cur);
-                Node up = cur.prev;
+                Node up = cur.prev;//반복조회방지를 위해 일단 한번 조회해서 로컬변수에 저장.
                 Node down = cur.next;
                 //링크 갱신
                 if(up!=null)
@@ -40,16 +40,13 @@ class EditingTable {
                 if(down!=null){
                     down.prev = cur.prev;
                     cur = cur.next;
-                } else cur = cur.prev;//down==null일 때 :마지막행일 때
+                } else cur = cur.prev;//cur = up;//down==null일 때 :마지막행일 때
             } else{//Z
                 Node node = mystack.pop();
-                Node up = cur.prev;
-                Node down = cur.next;
-                
-                if(up!=null)
-                    up.next = cur;
-                if(down!=null)
-                    down.prev = cur;
+                node.removed = false;
+
+                if(node.prev!=null) node.prev.next = node;
+                if(node.next!=null) node.next.prev = node;
             }
         }
         StringBuilder answer = new StringBuilder();
