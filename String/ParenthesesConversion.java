ee4589f96ea310f81d68ed28f515363eb57f2992
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-10-21 20:16:06 +0900

    Create ParenthesesConversion.java

---
+++ String/ParenthesesConversion.java
@@ -0,0 +1,34 @@
+class ParenthesesConversion {
+    static int pos;//u의 길이, v의 시작 인덱스
+    boolean isCorrect(String p){
+        boolean flag = true;
+        int sum = 0;
+        for(int i=0;i<p.length();i++){
+            if(p.charAt(i) == '(') sum++;
+            else sum--;
+            if(sum<0) flag = false;
+            if(sum==0){
+                pos = i+1;//최초로 sum이 0이되는 인덱스 i에 1더한값을 pos로 저장!
+                return flag;
+            }
+        }
+        
+        return flag;//이까지 내려올 일은 없지만
+    }
+    public String solution(String p) {
+        if(p.length()==0) return p;
+        boolean correct = isCorrect(p);
+        String u = p.substring(0,pos);
+        String v = p.substring(pos,p.length());
+        if(correct) return u+solution(v);
+        //4단계를 수행할 문자열 변수:answer를 사용!
+        String answer = "("+solution(v)+")";
+        StringBuilder finalS = new StringBuilder();
+        for(int i=1;i<u.length()-1;i++){
+            if(u.charAt(i) == '('){
+                answer+=')';
+            } else answer+='(';
+        }
+        return answer;
+    }
+}
