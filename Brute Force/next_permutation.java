e6474f7175d54955f975171ef47daaefea76d835
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-11-01 15:32:15 +0900

    Create next_permutation.java

---
+++ Brute Force/next_permutation.java
@@ -0,0 +1,50 @@
+package ss;
+import java.util.*;
+public class RearrangeNumber_Solution {
+	static boolean next_permutation(char[] a) {
+		int i = a.length-1;
+		while(i>0 && a[i-1] >= a[i]) {
+			i-=1;
+		}
+		if(i<=0) return false;
+		int j = a.length-1;
+		while(a[j] <= a[i-1]) {
+			j -= 1;
+		}
+		char temp = a[i-1];
+		a[i-1] = a[j];
+		a[j] = temp;
+		
+		j = a.length-1;
+		while(i<j) {
+			temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+			i+=1;
+			j-=1;
+		}
+		return true;
+	}
+	static int stoi(char[] a) {
+		int ans = 0;
+		for(char ch:a) {
+			ans = ans*10 + (ch - '0');
+		}
+		return ans;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char[] a = sc.next().toCharArray();//초기 시도와 맞음!!
+		int b = sc.nextInt();
+		int ans = -1;
+		Arrays.sort(a);
+		do {
+			int c = stoi(a);
+			if(a[0] != '0' && c<b) {
+				if(ans == -1 || ans < c) ans = c;
+			}
+		} while(next_permutation(a));
+		System.out.println(ans);
+	}
+
+}
