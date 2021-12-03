43a5daabd80d88813870de2a72a8f0a51717cb9b
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-12-03 18:42:03 +0900

    Update PartialSum.java

--- Brute Force/Two Pointer/PartialSum.java
+++ Brute Force/Two Pointer/PartialSum.java
@@ -1,30 +1,36 @@
 package ss;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class PartialSum {
-
-	public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int s = sc.nextInt();
+	static final int INF = 987654321;
+	public static void main(String args[]) throws Exception{
+        //n,s 입력ㄱ 받는다
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
         int[] a = new int[n+1];
+        
+        st = new StringTokenizer(br.readLine());
         for (int i=0; i<n; i++) {
-            a[i] = sc.nextInt();
+            a[i] = Integer.parseInt(st.nextToken());
         }
         int left=0;
         int right=0;
         int sum=a[0];
-        int ans=987654321;
+        int ans=INF;
         while (left <= right && right < n) {
             if (sum < s) {
                 right += 1;
                 sum += a[right];
             } else if (sum >= s) {
                 ans = Math.min(ans, right-left+1);
-                left = right; right = left+1;
-//                sum -= a[left];
-//                left++;
+                sum -= a[left];
+                left++;
             }
         }
+        if(ans == INF) ans = 0;
         System.out.println(ans);
     }
 
