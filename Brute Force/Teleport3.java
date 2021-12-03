0190d7db2afbe7ee1818028781faa77029024ce3
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-12-03 12:34:05 +0900

    Update Teleport3.java

--- Brute Force/Teleport3.java
+++ Brute Force/Teleport3.java
@@ -2,7 +2,7 @@ package ss;
 import java.util.*;
 import java.io.*;
 public class Teleport3 {
-	static int ans,sx,sy,ex,ey,Tele[][];
+	static int ans,sx,sy,ex,ey,path[][];
 	static final int INF = 987654321;
 	static class Info{
 		int x,y,t;
@@ -12,7 +12,7 @@ public class Teleport3 {
 	}
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
-	
+	static int[] a;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,61 +22,57 @@ public class Teleport3 {
 		ex = Integer.parseInt(st.nextToken());
 		ey = Integer.parseInt(st.nextToken());
 		
-		Tele = new int[3][4];
-		
-		for(int i=0;i<3;i++) {
+		path = new int[6][4];
+		a = new int[7];
+		for(int i=0;i<7;i++) a[i] = i;
+		for(int i=0;i<6;i+=2) {
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-			Tele[i][0] = x1; Tele[i][1] = y1; Tele[i][2] = x2; Tele[i][3] = y2;
+			path[i][0] = x1; path[i][1] = y1; path[i][2] = x2; path[i][3] = y2;
+			path[i+1][0] = x2; path[i+1][1] = y2; path[i+1][2] = x1; path[i+1][3] = y1;
 		}
-		ans = INF;
-		bfs(sx,sy);
-		System.out.println(ans);
-	}
-	static void bfs(int sx,int sy) {
-		Queue<Info> q = new LinkedList<>();
-		boolean[][][] visited = new boolean[2][1000000000][1000000000];
-		q.add(new Info(sx,sy,0));
-		visited[0][sx][sy] = visited[1][sx][sy] = true;//이 뜻은, 출발점으로 다시 이동하지 않겠다는 의미.
-		
-		while(!q.isEmpty()) {
-			Info cur = q.remove();
-			//정답 찾은 경우 : 쵯소값 갱신
-			if(cur.x == ex && cur.y == ey) {
-				ans = Math.min(ans, cur.t);
-			}
-			//텔레포트 시작점인 경우:텔레포트로 이동!
-			if(cur.x == Tele[0][0] && cur.y == Tele[0][1]) {
-				if(!visited[1][Tele[0][2]][Tele[0][3]]) {
-					q.add(new Info(Tele[0][2],Tele[0][3],cur.t+10));
-					visited[1][Tele[0][2]][Tele[0][3]] = true;
-				}
-			}
-			if(cur.x == Tele[1][0] && cur.y == Tele[1][1]){
-				if(!visited[1][Tele[1][2]][Tele[1][3]]) {
-					q.add(new Info(Tele[1][2],Tele[1][3],cur.t+10));
-					visited[1][Tele[1][2]][Tele[1][3]] = true;
-				}
+		do {
+			//현재 순열로 전체 이동 시간 구한다!
+			int nx = sx, ny = sy;
+			for(int i=0;i<7;i++) {
+				int dist = cal(nx,ny,path[i][0],path[i][1]);
+				if(path[i][2] == ex && path[i][3] == ey) break;
 				
+				dist+=10;
+				nx = path[i][2];ny = path[i][3];
 			}
-			if(cur.x == Tele[2][0] && cur.y == Tele[2][1]) {
-				if(!visited[1][Tele[2][2]][Tele[2][3]]) {
-					q.add(new Info(Tele[2][2],Tele[2][3],cur.t+10));
-					visited[1][Tele[2][2]][Tele[2][3]] = true;
-				}
-			}
-			else {
-				for(int dir=0;dir<4;dir++) {
-					int nx = cur.x+dx[dir], ny = cur.y+dy[dir];
-					if(!visited[0][nx][ny]) {
-						q.add(new Info(nx,ny,cur.t+1));
-						visited[0][nx][ny] = true;
-					}
-				}
-			}
-		}
+		}while(next_permutation());
 	}
+	static int cal(int x,int y,int dx,int dy) {
+		return Math.abs(x-dx)+Math.abs(y-dy);
+	}
+	static boolean next_permutation() {
+        int i = a.length-1;
+        while (i > 0 && a[i-1] >= a[i]) {
+            i -= 1;
+        }
+        if (i <= 0) return false;
+
+        int j = a.length-1;
+        while (a[j] <= a[i-1]) {
+            j -= 1;
+        }
+
+        int temp = a[i-1];
+        a[i-1] = a[j];
+        a[j] = temp;
+
+        j = a.length-1;
+        while (i < j) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i += 1;
+            j -= 1;
+        }
+        return true;
+    }
 }
