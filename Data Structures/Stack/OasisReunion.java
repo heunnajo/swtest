2c4bbbf83f95f0691382b6c0c9d83ac18047fc7a
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-12-22 13:41:22 +0900

    Update OasisReunion.java
    1. Before adding Pair object "p" to stack, increase current s.peek().n to "p".n. It was incorrect because increased s.peek().n not p.n

--- Data Structures/Stack/OasisReunion.java
+++ Data Structures/Stack/OasisReunion.java
@@ -1,7 +1,6 @@
-package ss;
 import java.util.*;
 import java.io.*;
-public class OasisReunion {
+public class Main {
 	static class Pair{
 		int h,n;
 		Pair(int h,int n){
@@ -17,23 +16,22 @@ public class OasisReunion {
 			a[i] = Integer.parseInt(br.readLine());
 		}
 		Stack<Pair> s = new Stack<>();
-		int ans = 0;
+		long ans = 0;
 		for(int i=0;i<n;i++) {
 			Pair p = new Pair(a[i],1);
 			while(!s.isEmpty()) {
 				if(s.peek().h <=a[i]) {
+                    ans+=(long)s.peek().n;
 					if(s.peek().h == a[i]) {
-						s.peek().n++;
+						p.n+=s.peek().n;
 					}
-					ans+=s.peek().n;
 					s.pop();
 				} else {
-					ans++;//이건 무조건 여기 들어강야할 것 같은딩!이 때는 1만 증가
+                    ans+= 1L;
 					break;
 				}
-				//공통적으로 ans++라면 if-else구문 밖에 한번만 넣어도 되지 않나?
 			}
-			s.push(p);//스택에 넣는 것은 공통 로직.그런데 top>h일 때에도 ans 증가시켜야함!
+			s.push(p);
 		}
 		System.out.println(ans);
 	}
