2f449763d137c55a3944cb4af34905d82b8305b8
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-12-24 13:51:23 +0900

    Create SayMiddle.java

---
+++ Data Structures/Heap/SayMiddle.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+public class SayMiddle {
+    static class Compare implements Comparator<Integer> {
+        public int compare(Integer one, Integer two) {
+            return two.compareTo(one);
+        }
+    }
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Compare cmp = new Compare();
+        PriorityQueue<Integer> l = new PriorityQueue<Integer>(1, cmp);
+        PriorityQueue<Integer> r = new PriorityQueue<>();
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (l.isEmpty() || r.isEmpty()) {
+                l.add(x);
+            } else {
+                if (x <= l.peek()) {
+                    l.add(x);
+                } else if (x >= r.peek()) {
+                    r.add(x);
+                } else {
+                    l.add(x);
+                }
+            }
+            //while (!(l.size() == r.size() || l.size() == r.size() + 1)) {
+            if (!(l.size() == r.size() || l.size() == r.size() + 1)) {
+                if (l.size() > r.size()) {
+                    r.add(l.peek());
+                    l.poll();
+                } else {
+                    l.add(r.peek());
+                    r.poll();
+                }
+            }
+
+            bw.write(l.peek() + "\n");
+        }
+        bw.flush();
+    }
+}
