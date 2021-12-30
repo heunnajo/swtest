eaebd517bf953e78a8fd9203037c6e09c3dde86a
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-12-30 09:52:15 +0900

    Create StringSet.java

---
+++ String/StringSet.java
@@ -0,0 +1,26 @@
+package ss;
+import java.util.*;
+import java.io.*;
+public class StringSet {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		Map<String,Integer> map = new HashMap<>();
+		while(n-->0) {
+			String s = br.readLine();
+			map.put(s, 1);
+		}
+		int ans = 0;
+		while(m-->0) {
+			String s = br.readLine();
+			if(map.containsKey(s)) {
+				ans++;
+			}
+		}
+		System.out.println(ans);
+	}
+
+}
