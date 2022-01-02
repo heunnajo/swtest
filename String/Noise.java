adf2c942f39ad6b73d3e0b3455b38ec614a07f37
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-01-03 00:46:52 +0900

    Solved by using String.

--- String/Noise.java
+++ String/Noise.java
@@ -1,15 +1,36 @@
-package ss;
 import java.io.*;
 import java.util.*;
 public class Noise {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int a = Integer.parseInt(br.readLine());
+		String a = br.readLine();
 		String op = br.readLine();
-		int b = Integer.parseInt(br.readLine());
-		if(op.equals("*")) System.out.println(a*b);
-		else System.out.println(a+b);
+		String b = br.readLine();
+		int a_len = a.length();//1을 제외한 0 갯수
+		int b_len = b.length();
+		StringBuilder sb = new StringBuilder();
+		if(op.equals("*")) {
+			sb.append("1");
+			for(int i=0;i<a_len+b_len-2;i++) {
+				sb.append("0");
+			}
+		} else {//덧셈
+			if(a_len<b_len) {//a<b:a가 더 작으면 b뒤에 a를 붙인다!
+				String tmp = b.substring(0, b_len-a_len);
+				sb.append(tmp).append(a);
+			} else if(a_len>b_len) {//a>b:b가 더 작으면 a뒤에 b붙인다!
+				String tmp = a.substring(0, a_len-b_len);
+				sb.append(tmp).append(b);
+			}
+			else if(a_len == b_len) {
+				sb.append("2");
+				for(int i=0;i<a_len-1;i++) {
+					sb.append("0");
+				}
+			}
+		}
+		System.out.println(sb);
 	}
 
 }
