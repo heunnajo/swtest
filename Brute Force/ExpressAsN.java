1ca511f4f3c34694de3dbf0d43edb097c8725212
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-01-11 10:45:56 +0900

    Update ExpressAsN.java
    Corrected wrong and missed part.
    Make a temporary variable to update N to NN, NNN, ...
    Change parameter "index" which means operator's index to "count" which means the number of N counts.

--- Brute Force/ExpressAsN.java
+++ Brute Force/ExpressAsN.java
@@ -1,35 +1,39 @@
-class ExpressAsN {
-    static int Num,N,tmp,ans;
+class Solution {
+    static int answer,N,Num;
     static final int INF = 987654321;
     public int solution(int n, int number) {
-        Num = number; N = n;//전역변수로 사용
-        tmp = INF;ans = INF;
-        
-        go(0,0);//ans에 최솟값을 저장
-        if(ans==INF) ans = -1;
-        return ans;
+        //전역변수로 설정!
+        N = n;
+        Num = number;//만들려고 하는 수
+        answer = INF;
+        go(0,0);//answer를 최솟값으로 갱신..왜 안 되나?
+        if(answer == INF) answer = -1;
+        return answer;
     }
-    //index:현재 선택할 연산자의 인덱스, sum:현재까지 계산한 값, cnt:N 사용 횟수
-    static void go(int index,int sum){
-        //1.재귀 종료 조건
-        if(index == 7){
-            if(sum != Num) ans = -1;//
-            //하지만 idx == 7까지 왔다는 건 num를 만들기 위해 왔는데 결국 sum != Num이면 문제 조건에 따라 그냥 ans=-1을 리턴:왜 자신감이 없나?ans에 넣지 않고 tmp에 넣다니
+    static void go(int count,int sum){
+        //1.종료 조건
+        //1-1. 불가능한 경우
+        if(count>8){
+            answer = -1;
             return;
         }
-        //2.정답 찾은 조건
+        //1-2. 정답 찾은 경우
         if(sum == Num){
-            int cnt = index+2;
-            if(cnt<ans) ans = cnt;
-            return;
+            if(answer == -1 || answer>count){
+                answer = count;
+                return;
+            }
         }
-        //3.현재 index의 연산자를 선택!
-        //4.다음 경우 호출
-        go(index+1,sum*10+N);
-        
-        go(index+1,sum+N);
-        go(index+1,sum-N);
-        go(index+1,sum*N);
-        go(index+1,sum/N);
+        //2.현재 깊이에서 선택
+        //3.다음 경우 호출
+        int x = N;
+        for(int i =0;i<8-count;i++){
+            int newCount = count+i+1;
+            go(newCount,sum+x);
+            go(newCount,sum-x);
+            go(newCount,sum*x);
+            go(newCount,sum/x);
+            x = x*10+N;
+        }
     }
 }
