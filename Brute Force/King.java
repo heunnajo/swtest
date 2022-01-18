e7b3dc617092fe953217ca3e1e1a242f42489efd
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-01-18 23:57:46 +0900

    Create King.java

---
+++ Brute Force/King.java
@@ -0,0 +1,13 @@
+package ss;
+import java.util.*;
+import java.io.*;
+public class King {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+
+	}
+
+}
