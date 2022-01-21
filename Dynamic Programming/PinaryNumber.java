d59c1012942e32475988f83f6853d9d919fc3eb3
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-01-21 23:30:24 +0900

    Create PinaryNumber.java

---
+++ Dynamic Programming/PinaryNumber.java
@@ -0,0 +1,18 @@
+package ss;
+import java.io.*;
+import java.util.*;
+public class PinaryNumber {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		long[] d = new long[n+1];
+		d[1] = 1; 
+		for(int i=2;i<=n;i++) {
+			d[i] = d[i-1]+d[i-2];
+		}
+		System.out.println(d[n]);
+	}
+
+}
