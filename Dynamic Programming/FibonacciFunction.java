f38e31cb05dfbcd3f80a90fa0bf03b382c0f5438
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-02-01 01:43:02 +0900

    Update FibonacciFunction.java

--- Dynamic Programming/FibonacciFunction.java
+++ Dynamic Programming/FibonacciFunction.java
@@ -1,4 +1,3 @@
-package ss;
 import java.io.*;
 import java.util.*;
 public class FibonacciFunction {
@@ -8,14 +7,15 @@ public class FibonacciFunction {
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		dp = new int[41];
-		cntZero = new int[41];
-		cntOne = new int[41];
 		Arrays.fill(dp, -1);
+		dp[0] = 0;dp[1] = 1;
 		fibo(40);
 		while(T-->0) {
 			int n = Integer.parseInt(br.readLine());
-			//int ans = fibo(n);
-			sb.append(cntZero[n]+" "+cntOne[n]+"\n");
+			//n-1에 따라 분기처리
+			if(n==0) sb.append(1+" "+0+"\n");
+			else if(n==1) sb.append(0+" "+1+"\n");
+			else sb.append(dp[n-1]+" "+dp[n]+"\n");
 		}
 		System.out.println(sb);
 				
@@ -24,11 +24,11 @@ public class FibonacciFunction {
 		//이미 구한 것은 다시 구하지 않는다!
 		if(dp[n] != -1) return dp[n];
 		if(n == 0) {
-			cntZero[n]++;
+			//cntZero[n]++;
 			return 0;
 		}
 		if(n == 1) {
-			cntOne[n]++;
+			//cntOne[n]++;
 			return 1;
 		}
 		return dp[n] = fibo(n-1)+fibo(n-2);
