9f7f17f31522dfe8dc85b35bf04089091c01e365
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-01-31 23:08:36 +0900

    Create FibonacciFunction.java

---
+++ Dynamic Programming/FibonacciFunction.java
@@ -0,0 +1,37 @@
+package ss;
+import java.io.*;
+import java.util.*;
+public class FibonacciFunction {
+	static int dp[],cntZero[],cntOne[];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		dp = new int[41];
+		cntZero = new int[41];
+		cntOne = new int[41];
+		Arrays.fill(dp, -1);
+		fibo(40);
+		while(T-->0) {
+			int n = Integer.parseInt(br.readLine());
+			//int ans = fibo(n);
+			sb.append(cntZero[n]+" "+cntOne[n]+"\n");
+		}
+		System.out.println(sb);
+				
+	}
+	static int fibo(int n) {
+		//이미 구한 것은 다시 구하지 않는다!
+		if(dp[n] != -1) return dp[n];
+		if(n == 0) {
+			cntZero[n]++;
+			return 0;
+		}
+		if(n == 1) {
+			cntOne[n]++;
+			return 1;
+		}
+		return dp[n] = fibo(n-1)+fibo(n-2);
+	}
+
+}
