6024665faefae5fc2834d05149da941e8412c910
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-02-05 23:43:17 +0900

    Create NumberOfPaper.java

---
+++ Implementation/NumberOfPaper.java
@@ -0,0 +1,11 @@
+package ss;
+import java.io.*;
+import java.util.*;
+public class NumberOfPaper {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
