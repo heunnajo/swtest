3e7af4b9c80ccc5e3a04054a6c53efca1c7162ed
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-02-06 23:04:27 +0900

    Create ATM.java

---
+++ Implementation/ATM.java
@@ -0,0 +1,16 @@
+package ss;
+import java.io.*;
+import java.util.*;
+public class ATM {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		while(n-- >0) {
+			int a = Integer.parseInt(st.nextToken());
+		}
+
+	}
+
+}
