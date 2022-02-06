584e203f42db6165a8dcfcd41920b536927a84bc
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-02-07 00:04:56 +0900

    Update ATM.java

--- Implementation/ATM.java
+++ Implementation/ATM.java
@@ -1,16 +1,28 @@
 package ss;
 import java.io.*;
 import java.util.*;
+
 public class ATM {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		while(n-- >0) {
-			int a = Integer.parseInt(st.nextToken());
+		int[] time = new int[n];
+		int[] d = new int[n];
+		for(int i=0;i<n;i++) {
+			time[i] = Integer.parseInt(st.nextToken());
 		}
-
+		Arrays.sort(time);
+		int sum = 0;
+		for(int i=0;i<n;i++) {
+			sum += time[i];
+			d[i] = sum;
+		}
+		int answer = 0;
+		for(int i=0;i<n;i++) answer += d[i];
+		
+		System.out.println(answer);
 	}
 
 }
