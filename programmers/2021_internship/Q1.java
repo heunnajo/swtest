cde1561471c5b796bf247d7b77491d188c26dd6b
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-02-11 16:41:00 +0900

    Create Q1.java

---
+++ programmers/2021_internship/Q1.java
@@ -0,0 +1,12 @@
+//1. 숫자 문자열과 영단어
+class Solution {
+    public int solution(String s) {
+        String[] words = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        String[] nums = {"0","1","2","3","4","5","6","7","8","9"};
+        
+        for(int i=0;i<10;i++){
+            s = s.replace(words[i],nums[i]);
+        }
+        return Integer.parseInt(s);
+    }
+}
