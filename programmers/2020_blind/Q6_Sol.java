2e705cb5a27c9aaf865a309d354d82065762f7f4
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-02-21 20:10:41 +0900

    Update Q6_Sol.java

--- programmers/2020_blind/Q6_Sol.java
+++ programmers/2020_blind/Q6_Sol.java
@@ -2,8 +2,8 @@
 import java.util.*;
 
 class Solution {
-    //재귀함수로 완전 탐색을 구현할 것. 가급적 로컬 변수를 줄이는 것이 좋기 때문에 전역변수로 선언합니다~
-    static final int INF = 987654321;//코드리뷰Q. 왜 final로 선언했나요?
+    //재귀함수로 완전 탐색을 구현할 것. 가급적 로컬 변수를 줄이는 것이 좋기 때문에 전역변수로 선언
+    static final int INF = 987654321;
     int N,MinCnt;
     int[] Weak;
     int[] Dist;
@@ -15,9 +15,7 @@ class Solution {
         if(cnt >= MinCnt) return;//작은 값을 찾는 것이 의미있기 때문!모ㅗ든 취약점 방문 방법x필요한 최소 친구수만 찾기 때문에!
             
         for(int i=0;i<Weak.length;i++){
-            int nextPos = (pos + i) % Weak.length;//최초 nextPos = pos + 0 으로서, 시작위치부터 방문할 수 있다!!?
-            //인덱스 초과 어떻게 핸들링할 것인가? 취약지점 갯수가 4라면 이 갯수를 넘어간다면 넘어가는 만큼 뒤로 rewind해줘야한다!=> 모듈러 연산. 내가 생각했던 부분임.
-            //시작점부터의 거리 계산???왜?
+            int nextPos = (pos + i) % Weak.length;//최초 nextPos = pos + 0 으로서, 시작위치부터 방문할 수 있다!
             int diff = Weak[nextPos] - Weak[pos];//이동하고자 하는 취약점 위치값 - 시작 위치값.최초에는 동일하기 때문에 diff = 0이 됨.
             
             if(nextPos < pos){
@@ -27,18 +25,16 @@ class Solution {
             if(diff > Dist[Dist.length-cnt]){//이동할 수 있는 거리보다 크면 break;거리는 가장 긴 것부터 쓴다!
                 break;
             }
-            visited |= 1 << nextPos;//해당하는 비트만 1로 켜지게 된다.
+            visited |= 1 << nextPos;
         }
         //다 마킹한 다음! 모든 취약점 다 방문했는지 확인!:취약점 갯수만큼의 비트가 다 1로 켜진다!
-        //여기까지 왓따는 건 다 자기짝을 찾았다는 뜻
         if(visited == (1<<Weak.length)-1){
             MinCnt = Math.min(MinCnt,cnt);
             return ;
         }
         for(int i=0;i<Weak.length;i++){
-            if((visited & (1<<i)) != 0) continue;//이미 방문. 시작 위치로 사용
-            
-            solve(cnt+1,i,visited);// 
+            if((visited & (1<<i)) != 0) continue;
+            solve(cnt+1,i,visited);
         }
         
     }
@@ -50,7 +46,7 @@ class Solution {
         MinCnt = INF;
         
         for(int i=0;i<Weak.length;i++){
-            solve(1,i,0);//친구 마다 시작점을 다르게 해서 완전 탐색하는 건데! 2중 for문으로 친구 반복문, 시작점 반복문 구현해야되는 것 아닌가..??!  
+            solve(1,i,0);
         }
         if(MinCnt == INF) return -1;
         return MinCnt;
