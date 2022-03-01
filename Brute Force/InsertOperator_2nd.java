fd9e72a0ef1c78f5ccc99a3ebd1a6c7334eef806
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-03-01 20:23:30 +0900

    Update InsertOperator_2nd.java

--- Brute Force/InsertOperator_2nd.java
+++ Brute Force/InsertOperator_2nd.java
@@ -1,21 +1,17 @@
 // 백준 14888번. 연산자 끼워넣기
-package ss;
 import java.util.*;
 import java.io.*;
-public class InsertOperator_4th {
+public class Main {
 	static int n;
 	static ArrayList<Integer> nums;
-	//static ArrayList<String> ops;
 	static int[] ops;
-	//static long max,min;//정답 저장 변수
 	static int max,min;//정답 저장 변수
-	static String[] realOps = {"+","-","*","/"};
+	static char[] realOps = {'+','-','*','/'};
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		nums = new ArrayList<>();
-		//ops = new ArrayList<>();
 		
 		n = Integer.parseInt(br.readLine());
 		ops = new int[4];
@@ -28,52 +24,53 @@ public class InsertOperator_4th {
 		for(int i=0;i<4;i++)
 			ops[i] = Integer.parseInt(st.nextToken());
 		
-		ArrayList<String> selected = new ArrayList<>();
-		go(0,selected);
+		max = Integer.MIN_VALUE; min = Integer.MAX_VALUE;
+		go(0,new char[n-1]);
+		System.out.println(max);
+		System.out.println(min);
 	}
 	
 	//idx:현재 선택하는 연산자의 인덱스
 	//selected:현재까지 선택한 연산자 정보
-	static void go(int idx, ArrayList<String> selected) {
+	static void go(int idx, char[] selected) {
 		//1.종료 조건 :n-1개 연산자 다 선택한 경우
 		if(idx == n-1) {
-			int res = operate(selected);
-			max  =  max < res ? max : res;
-			min  =  min > res ? min : res;
+			//확인O
+//			for(int i=0;i<n-1;i++) System.out.print(selected[i]+" ");
+//			System.out.println();
+			operate(selected);//max,min 결과 갱신!
 			return;
 		}
-		//2.현재 선택, 다음 경우 호출 : 선택정보를 배열에 저장, O(1)로 조회해야할까?
+		//2.현재 선택, 다음 경우 호출 : 선택정보를 배열에 저장
 		for(int i=0;i<4;i++) {
-			if(ops[i]==0) continue;
-			ops[i]--; selected.add(realOps[i]);
-			System.out.println("add 후 selected 크기: "+selected.size());//왜 값이 다르게 나오는 거야?????
-			
+			if(ops[i]==0) continue;//갯수가 0이면 선택하지 않고 컨티뉴처리!
+			ops[i]--; selected[idx] = realOps[i];
 			go(idx+1,selected);
 			ops[i]++; 
-			System.out.println("selected 크기: "+selected.size());
-			selected.remove(selected.size()-1);//원복.왜지?왜 여기서 -1? 하나 추가한 원소를 삭제하는 건데  
 		}
 	}
 	//숫자 정보:전역변수 nums에서 복사해서 조회
 	//연산자 순서 정보:매개변수 selected에서 조회
-	static int operate(ArrayList<String> selected) {
+	static void operate(char[] selected) {
 		ArrayList<Integer> copiedNums = new ArrayList<>(nums);
-		
-		int ans = 0;
-		for(int i=0;i<selected.size();i++) {
-			ans = cal(copiedNums.remove(i),copiedNums.remove(i),selected.remove(i));
-			copiedNums.add(i,ans);
-			i--;
+		int res = 0;
+		int idx=0;
+		for(int i=0;i<selected.length;i++) {
+			res = cal(copiedNums.remove(idx),copiedNums.remove(idx),selected[i]);
+			copiedNums.add(idx,res);
 		}
-		return copiedNums.get(0);
+		//계산하고 난 후 최종 결과값을 최솟값, 최댓값 갱신시켜준다!
+        res = copiedNums.get(0);
+		max  =  max < res ? res : max;
+		min  =  min > res ? res : min;
 	}
-	static int cal(int n1,int n2,String op) {
+	static int cal(int n1,int n2,char op) {
 		int sum = 0;
-		if(op.equals("+")) {
+		if(op == '+') {
 			return n1+n2;
-		} else if(op.equals("-")) {
+		} else if(op == '-') {
 			return n1-n2;
-		} else if(op.equals("*")) {
+		} else if(op == '*') {
 			return n1*n2;
 		} else {//"/"
 			return n1/n2;
@@ -81,22 +78,3 @@ public class InsertOperator_4th {
 	}
 
 }
-
-//제대로 순열을 생성하는지 확인:O
-//for(String s: selected) {
-//	System.out.print(s+" ");
-//}
-//System.out.println();
-//return;
-
-//		for(int i=0;i<n;i++)
-//			System.out.print(nums.get(i)+" ");
-//		System.out.println();
-//		
-//		
-//		for(int i=0;i<4;i++)
-//			System.out.print(ops[i]+" ");
-//		System.out.println();
-
-
-//System.out.println("n: "+n);
