4560ce0b9d787db9cb5c4fb26f76095c25035ce3
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-03-02 13:19:51 +0900

    Update Q3_2nd.java

--- programmers/2020_internship/Q3_2nd.java
+++ programmers/2020_internship/Q3_2nd.java
@@ -17,14 +17,6 @@ class Solution {
         
         //2.알고리즘 : 투포인터로 gems 순회
         while(true){
-            //모든 보석 종류 map에 기록 : 구간 길이 계속 늘인다 : right++
-            //map.put(gems[right],map.getOrDefault(0,map.get(gems[right])+1));
-            map.put(gems[right],map.getOrDefault(gems[right],0)+1));
-            right++;
-            
-            //인덱스 범위 체크
-            if(right >= len) break;
-            
             //정답 찾은 경우 : 구간 길이 최솟값 도출 위해 구간 줄인다 : left++
             if(map.size() == n){
                 //구간 길이 최솟값 도출
@@ -36,9 +28,17 @@ class Solution {
                 map.put(gems[left],map.get(gems[left])-1);
                 if(map.get(gems[left]) == 0) map.remove(gems[left]);
                 left++;
+            } 
+            else if(right == len) break;//인덱스 범위 체크
+            else{
+                //모든 보석 종류 map에 기록 : 구간 길이 계속 늘인다 : right++
+                //map.put(gems[right],map.getOrDefault(0,map.get(gems[right])+1));
+                //map.put(gems[right],map.getOrDefault(0,gems[right])+1);
+                map.put(gems[right],map.getOrDefault(gems[right],0)+1);
+                right++;
             }
         }        
-        
+        answer[0] = ans_left; answer[1] = ans_right;
         return answer;
     }
 }
