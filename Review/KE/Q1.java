48ec97627fe1e6a1b636f09553ac078f630942c0
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-03-07 20:34:29 +0900

    Update Q1.java

---
+++ Review/KE/Q1.java
@@ -0,0 +1,12 @@
+//1.게임 개발 과제:2차원 배열 탐색 BFS+완탐?!
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken()); int n = Integer.parseInt(st.nextToken());
+		System.out.print(m*n);
+	}
+	//System.out.print("m: "+m+" n: "+n);
+}
