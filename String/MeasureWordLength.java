5edaa46065be24068b6eef05b6b8fd4323edae65
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-03-13 22:47:45 +0900

    Create MeasureWordLength.java

---
+++ String/MeasureWordLength.java
@@ -0,0 +1,12 @@
+//백준 2743. 단어 길이 재기
+import java.util.*;
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println(s.length());
+	}
+
+}
