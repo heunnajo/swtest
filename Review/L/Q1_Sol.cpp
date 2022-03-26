ffc9cce3a5f37b23be2d3b15baab453b7af30e80
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-03-26 19:37:46 +0900

    Update Q1_Sol.cpp

--- Review/L/Q1_Sol.cpp
+++ Review/L/Q1_Sol.cpp
@@ -8,17 +8,25 @@ using namespace std;
 #include <regex>
 
 //team_name : teamOne application_name : line error_level : serious message : Fuck
+//team_name : team123 application_name : line error_level : serious message : Fuck
+//team_name : teamOne application_name : line error_level : serious!!! message : Fuck
+//     team_name : teamOne application_name : line error_level : serious message : Fuck
+//team_name : teamOne application_name : line error_level : serious message : Fuck       
+//team_name: teamOne application_name : line error_level : serious message : Fuck
+//team_name : teamOne application_name : line error_level : ThisIsVeryVerySeriousSoThatYouNeedToFixItRrightAwayNoMatterWhatYouAreDoingNowYouNeedToComeToOfficeRIGHTNOW message : Fuck
 
 int N;
 regex re("^team_name : [a-zA-Z]+ application_name : [a-zA-Z]+ error_level : [a-zA-Z]+ message : [a-zA-z]+$");
 void Input(){
-    cin >> N;
+    cin >> N >> '\n';
+    cin.ignore();
 }
 
 void Pro() {
     int ans = 0;
+    string str;
     for(int i=1;i<=N;i++){
-        string str;
+        //string str;
         getline(cin,str);
         if(!(regex_match(str,re)) ans += 1;
     }
