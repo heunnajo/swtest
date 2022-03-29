c71382897b94e36527ca08c16a2c856c542f56c9
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-03-29 20:15:26 +0900

    Update Q1_2nd_Solved.java

--- Review/L/Q1_2nd_Solved.java
+++ Review/L/Q1_2nd_Solved.java
@@ -2,6 +2,17 @@
 import java.io.*;
 import java.util.*;
 
+/*
+로그 조건만족 : 0 로그 조건 불만족 : 1
+0 team_name : teamOne application_name : line error_level : serious message : Fuck//로그 수집 조건 만족
+1 team_name : team123 application_name : line error_level : serious message : Fuck//숫자 포함
+1 team_name : teamOne application_name : line error_level : serious!!! message : Fuck//특수문자포함
+1    team_name : teamOne application_name : line error_level : serious message : Fuck//앞에 공백문자
+1 team_name : teamOne application_name : line error_level : serious message : Fuck       //뒤에 공백문자
+1 team_name: teamOne application_name : line error_level : serious message : Fuck//" : "형태 불만족ㄱ
+1 team_name : teamOne application_name : line error_level : ThisIsVeryVerySeriousSoThatYouNeedToFixItRrightAwayNoMatterWhatYouAreDoingNowYouNeedToComeToOfficeRIGHTNOW message : Fuck//로그 길이 100자 이상.
+*/
+
 public class Q1_2nd_Solved {
 
 	public static void main(String[] args) throws Exception {
