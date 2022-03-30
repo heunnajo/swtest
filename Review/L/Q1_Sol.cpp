33a7c757fcb42c6171bd9e6ac95d15381a44a441
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-03-30 21:00:02 +0900

    Update Q1_Sol.cpp

--- Review/L/Q1_Sol.cpp
+++ Review/L/Q1_Sol.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 #include <regex>
 
-//team_name : teamOne application_name : line error_level : serious message : Fuck
-//team_name : team123 application_name : line error_level : serious message : Fuck
-//team_name : teamOne application_name : line error_level : serious!!! message : Fuck
+// team_name : teamOne application_name : line error_level : serious message : Fuck
+// team_name : team123 application_name : line error_level : serious message : Fuck
+// team_name : teamOne application_name : line error_level : serious!!! message : Fuck
 //     team_name : teamOne application_name : line error_level : serious message : Fuck
-//team_name : teamOne application_name : line error_level : serious message : Fuck       
-//team_name: teamOne application_name : line error_level : serious message : Fuck
-//team_name : teamOne application_name : line error_level : ThisIsVeryVerySeriousSoThatYouNeedToFixItRrightAwayNoMatterWhatYouAreDoingNowYouNeedToComeToOfficeRIGHTNOW message : Fuck
+// team_name : teamOne application_name : line error_level : serious message : Fuck       
+// team_name: teamOne application_name : line error_level : serious message : Fuck
+// team_name : teamOne application_name : line error_level : ThisIsVeryVerySeriousSoThatYouNeedToFixItRrightAwayNoMatterWhatYouAreDoingNowYouNeedToComeToOfficeRIGHTNOW message : Fuck
 
 int N;
 regex re("^team_name : [a-zA-Z]+ application_name : [a-zA-Z]+ error_level : [a-zA-Z]+ message : [a-zA-z]+$");
