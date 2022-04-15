05d5a6dd3d54d936e91c574375339a2b4a120b74
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-04-15 20:59:53 +0900

    Create AdividebyB.java

---
+++ Implementation/AdividebyB.java
@@ -0,0 +1,15 @@
+//BOJ#1008 A/B
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double A,B;
+		Scanner sc = new Scanner(System.in);
+		A = (double)sc.nextInt();
+		B = (double)sc.nextInt();
+		System.out.println(A/B);
+	}
+	
+}
