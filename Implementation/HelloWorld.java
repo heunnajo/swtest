1b634c5c26dad49bc1963064866b75956bd0f2e3
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-04-16 22:41:14 +0900

    Create HelloWorld.java

---
+++ Implementation/HelloWorld.java
@@ -0,0 +1,11 @@
+//BOJ #2557 Hello World
+import java.util.*;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+	
+}
