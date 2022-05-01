9a68d6fd8512a07d6cd6b8d1728118f2ee866ab5
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-05-01 23:59:04 +0900

    Update SelectChairman.java

--- Graph/SelectChairman.java
+++ Graph/SelectChairman.java
@@ -7,15 +7,15 @@ import java.util.StringTokenizer;
  
 public class Main {
     static final int INF = 987654321;
- 
+    static int N,arr[][];
+    
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
  
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
  
-        int[][] arr = new int[N + 1][N + 1];
+        arr = new int[N + 1][N + 1];
  
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
@@ -27,28 +27,17 @@ public class Main {
  
         while (true) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
  
-            if (x == -1 && y == -1) {
-                break;
-            }
+            if (u == -1 && v == -1) break;
  
-            arr[x][y] = arr[y][x] = 1; // 친구 관계는 양방향
+            arr[u][v] = arr[v][u] = 1; // 친구 관계는 양방향
         }
  
-        // 플로이드 와샬 알고리즘 수행
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
-                        arr[i][j] = arr[i][k] + arr[k][j];
-                    }
-                }
-            }
-        }
+        FloydWarshall();
  
-        int readerScore = INF;
+        int leaderScore = INF;
  
         int[] scoreArr = new int[N + 1]; // 친구 점수 목록
  
@@ -61,29 +50,36 @@ public class Main {
             }
  
             scoreArr[i] = score;
-            readerScore = Math.min(readerScore, score);
+            leaderScore = Math.min(leaderScore, score);
         }
  
         StringBuilder title = new StringBuilder();
-        title.append(readerScore + " ");
+        title.append(leaderScore + " ");
  
-        int readerNum = 0;
+        int leaderNum = 0;
  
         StringBuilder body = new StringBuilder();
         for (int i = 1; i <= N; i++) {
-            if (readerScore == scoreArr[i]) {
-                readerNum++;
+            if (leaderScore == scoreArr[i]) {
+                leaderNum++;
                 body.append(i + " ");
             }
         }
  
-        title.append(readerNum + "\n");
+        title.append(leaderNum + "\n");
  
-        bw.write(title.toString());
-        bw.write(body.toString() + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(title);
+        System.out.print(body + "\n");
     }
- 
+    static void FloydWarshall() {
+    	for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+    }
 }
