8d4d4d4728d75c39d33dfb868c2de1662aef737b
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-05-30 23:42:22 +0900

    Update BOJ1059.java

--- Brute Force/BOJ1059.java
+++ Brute Force/BOJ1059.java
@@ -1,4 +1,42 @@
 //좋은 구간
-public class BOJ1059 {
-    
+package ss;
+import java.io.*;
+import java.util.*;
+public class BOJ1059_Sol {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int l = Integer.parseInt(br.readLine());
+		int[] arr = new int[l];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i=0;i<l;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int n = Integer.parseInt(br.readLine());
+		
+		Arrays.sort(arr);
+		
+		int start = -1, end = -1;
+		for(int i=0;i<l;i++) {
+			if(arr[i]<n) start = arr[i];
+			else if(arr[i]>n) {
+				end = arr[i];
+			}
+			else {
+				System.out.println("0");
+				return;
+			}
+			if(start != -1 && end != -1) break;
+		}
+		if(start == -1) start = 1;
+		if(end == -1) end = 1000;
+		
+		int ans = (n-start) * (end-n) -1;
+		System.out.println(ans);
+	}
+
 }
+
