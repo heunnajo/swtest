1723b89c4cf13ceb20bf0a0238ec8ac898863be0
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-07-03 12:01:34 +0900

    Create Q3.java

---
+++ Review/DevMatch/2022_1Q_2/Q3.java
@@ -0,0 +1,49 @@
+//bfs
+class Solution {
+    int[] dx = {-1,0,1,0};
+    int[] dy = {0,1,0,-1};
+    int N,ans[][];
+    boolean H;
+    boolean isOut(int x,int y){
+        return x<0 || x>=N || y<0 || y>=N;
+    }
+    public int[][] solution(int n, boolean horizontal) {
+        N = n;
+        H = horizontal;
+
+        ans = new int[n][n];
+
+        solve();
+
+        return ans;
+    }
+    void solve(){
+        int amt = 1;
+        int dir = 1;
+        int num = 1;
+        int x = 0; int y = 0;
+        ans[x][y] = num++;
+
+        if(H){//수평
+            while(true){
+                for(int i=0;i<amt;i++){
+                    x += dx[dir]; y += dy[dir];
+                    ans[x][y] = num++;
+                }
+                //방향 전환
+                dir = (dir+1)%4;
+                int nx = x+dx[dir]; int ny = y+dy[dir];
+                if(ans[nx][ny] != 0) dir = (dir+2)%4;
+
+                amt++;
+
+                nx = x+dx[dir]; ny = y+dy[dir];
+                if(ans[nx][ny] != 0 && isOut(nx,ny)) break;
+            }
+
+
+        } else{
+
+        }
+    }
+}
