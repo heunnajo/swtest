36e882ce642e9874b7768882d931b196af8e1815
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-10-28 10:23:22 +0900

    Create Q5.sql

---
+++ SQL/programmers/SELECT/Q5.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+/*흉부외과 또는 일반외과 의사 목록 출력하기*/
+/*DATE_FORMAT(DATETIME, '%Y-%m-%d') AS 날짜*/
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD,'%Y-%m-%d') AS HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD IN ('CS','GS')
+ORDER BY HIRE_YMD DESC, DR_NAME
