7d29336db95a8dd0c73c6a5e765a0b75bc05bbb3
heunnajo <61869540+heunnajo@users.noreply.github.com>
2022-11-23 16:14:30 +0900

    Create Q133502_Stack.java

---
+++ programmers/Q133502_Stack.java
@@ -0,0 +1,16 @@
+//#133502 햄버거 만들기
+
+import java.util.*;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> st = new Stack<>();
+        
+        int n = ingredient.length;
+        for(int i=0;i<n;i++){
+            
+        }
+        
+        return answer;
+    }
+}
