7373aa9dc18864357a90e1bb53822bd7df4678c8
heunnajo <61869540+heunnajo@users.noreply.github.com>
2023-01-14 11:56:56 +0900

    Create Q1.java

---
+++ Review/SH/2023/Q1.java
@@ -0,0 +1,18 @@
+//걍 몰겠음..
+class Solution {
+    public int[] solution(int[][] recruits, int s1, int s2) {
+        int[] answer = new int[2];
+        int junior = 0; int senior = 0; int expert = 0;
+        int n = recruits.length;
+
+        int curCareer,curScore;
+        for(int i=0;i<n;i++){
+            curCareer = recruits[i][0];
+            curScore = recruits[i][1];
+
+            //조건 판단
+
+        }
+        return answer;
+    }
+}
