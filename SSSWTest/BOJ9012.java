06047236620566e9d6e03dae6b680a345bf9ea58
heunnajo <61869540+heunnajo@users.noreply.github.com>
2023-07-22 21:01:21 +0900

    Update BOJ9012.java

--- SSSWTest/BOJ9012.java
+++ SSSWTest/BOJ9012.java
@@ -1,11 +1,8 @@
-package barkingAlgo;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
 //괄호
-//고쳐야함
 public class BOJ9012 {
 
 	public static void main(String[] args) throws Exception {
@@ -16,24 +13,18 @@ public class BOJ9012 {
 		int len;
 		Stack<Character> st;
 		int cnt = 0;
-		boolean isVPS = true;
+		boolean isVPS;//true로  초기화해서 틀렸었음.
 		StringBuilder sb = new StringBuilder();
 		while(TC-- >0) {
 			word = br.readLine();
 			len = word.length();
 			st = new Stack<>();
+			isVPS = true;
 			
 			for(int i=0;i<len;i++) {
 				if(word.charAt(i) == '(') {
 					st.push(word.charAt(i));
 				} else {//)
-//					if(st.isEmpty()) {//현재 문자열이 올바른 문자열이 아닌 경우!
-//						isVPS = false;
-//						break;
-//					}
-//					if(!st.isEmpty() && st.peek() == '(') {
-//						st.pop();
-//					}
 					if(st.isEmpty() || st.peek() != '(') {//현재 문자열이 올바른 문자열이 아닌 경우!
 						isVPS = false;
 						break;
@@ -42,7 +33,6 @@ public class BOJ9012 {
 				}
 			}
 			
-			//if(st.isEmpty()) {isVPS = true;}
 			if(!st.isEmpty()) {isVPS = false;}
 			if(isVPS) {sb.append("YES").append("\n");}
 			else {sb.append("NO").append("\n");}
