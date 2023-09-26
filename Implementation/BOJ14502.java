4eac6e197dc7fe56070b910be447c4ebb88eddf2
heunnajo <61869540+heunnajo@users.noreply.github.com>
2023-09-26 23:26:51 +0900

    Update BOJ14502.java

--- Implementation/BOJ14502.java
+++ Implementation/BOJ14502.java
@@ -3,9 +3,9 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-//연구소 : debugging
+//연구소
 public class BOJ14502 {
-	static int ans,N,M,Map[][];
+	static int ans,N,M,Map[][],copiedMap[][];
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
 	static boolean[][] Visited;
@@ -24,6 +24,7 @@ public class BOJ14502 {
 		M = Integer.parseInt(input[1]);
 		
 		Map = new int[N][M];
+		copiedMap = new int[N][M];
 		
 		for(int i=0;i<N;i++) {
 			input = br.readLine().split(" ");
@@ -39,6 +40,7 @@ public class BOJ14502 {
 		//1.종료
 		if(idx == 3) {
 			//print();
+			copyMap();
 			spread();//bfs
 			int cnt = countZero();
 			if(cnt > ans) {
@@ -51,12 +53,20 @@ public class BOJ14502 {
 		for(int i=st;i<N*M;i++) {
 			int r = i / M; int c = i % M;
 			if(Map[r][c] == 0) {
-				Map[r][c] = -1;
+				Map[r][c] = 1;
 				go(idx+1,i+1);
 				Map[r][c] = 0;
 			}
 		}
 	}
+	// 기존 맵을 유지하기 위해 바이러스 퍼트릴 맵 복사하기
+    static void copyMap() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                copiedMap[i][j] = Map[i][j];
+            }
+        }
+    }
 	static void print() {
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<M;j++) {
@@ -70,7 +80,7 @@ public class BOJ14502 {
 		Visited = new boolean[N][M];
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<M;j++) {
-				if(Map[i][j] == 2 && !Visited[i][j]) {//미방문한 2인칸 방문, 확산시킨다!
+				if(copiedMap[i][j] == 2 && !Visited[i][j]) {//미방문한 0인칸 방문, 확산시킨다!
 					bfs(i,j);
 				}
 			}
@@ -86,10 +96,13 @@ public class BOJ14502 {
 			for(int d=0;d<4;d++) {
 				int nx = cur.x+dx[d];
 				int ny = cur.y+dy[d];
-				if(isOut(nx,ny) || Map[nx][ny] != 2) continue;
+				if(isOut(nx,ny)) continue;
 				
-				q.offer(new Pair(nx,ny));
-				Visited[nx][ny] = true;
+				if(copiedMap[nx][ny] == 0) {
+					q.offer(new Pair(nx,ny));
+					Visited[nx][ny] = true;
+					copiedMap[nx][ny] = 2;
+				}
 			}
 		}
 	}
@@ -100,7 +113,7 @@ public class BOJ14502 {
 		int sum = 0;
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<M;j++) {
-				if(Map[i][j] == 0) sum++;
+				if(copiedMap[i][j] == 0) sum++;
 			}
 		}
 		return sum;
